82be89d4285da2ea742c4b1d08085943a9189993
Sunil Vaishnav <sunil@gmail.com>
2025-10-03 22:37:23 +0530

    create migrations

---
+++ database/migrations/2025_10_03_170419_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('submission_id');
+            $table->integer('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->enum('provider', ['razorpay', 'stripe']);
+            $table->string('transaction_id')->nullable();
+            $table->enum('status', ['success', 'failed', 'pending'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
